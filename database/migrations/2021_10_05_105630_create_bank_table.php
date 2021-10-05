dca19b87fb4ccc8ed1eeaa293fa9866a13053f97
Jireii <mazhn34@gmail.com>
2021-10-05 19:24:41 +0800

    added tables: bank, wallet, promo

--- database/migrations/2021_10_05_105630_create_bank_table.php
+++ database/migrations/2021_10_05_105630_create_bank_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateBankTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('bank', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid', 36)->unique();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->char('uuid', 36)->unique();
+            $table->string('name_bank');
+            $table->string('code_bank');
+            $table->string('number_bank');
+            $table->string('method_bank');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -32,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('bank');
     }
 }
