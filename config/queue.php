b23eabcfa9ab74f3bb87ff4cb1346ace603d992c
miftahul mulki <61178290+mulki12@users.noreply.github.com>
2021-11-10 14:53:00 +0700

    anjasmara_service_wallet

--- config/queue.php
+++ config/queue.php
@@ -70,9 +70,49 @@ return [
             'block_for' => null,
             'after_commit' => false,
         ],
+        'rabbitmq' => [
+
+            'driver' => 'rabbitmq',
+            'queue' => env('RABBITMQ_QUEUE', 'wallets'),
+            'connection' => PhpAmqpLib\Connection\AMQPLazyConnection::class,
+
+            'hosts' => [
+                [
+                    'host' => env('RABBITMQ_HOST', '127.0.0.1'),
+                    'port' => env('RABBITMQ_PORT', 5672),
+                    'user' => env('RABBITMQ_USER', 'guest'),
+                    'password' => env('RABBITMQ_PASSWORD', 'guest'),
+                    'vhost' => env('RABBITMQ_VHOST', 'guest'),
+                    'queue' => env('RABBITMQ_QUEUE', 'wallets'),
+                ],
+            ],
+
+            'options' => [
+                'ssl_options' => [
+                    'cafile' => env('RABBITMQ_SSL_CAFILE', null),
+                    'local_cert' => env('RABBITMQ_SSL_LOCALCERT', null),
+                    'local_key' => env('RABBITMQ_SSL_LOCALKEY', null),
+                    'verify_peer' => env('RABBITMQ_SSL_VERIFY_PEER', true),
+                    'passphrase' => env('RABBITMQ_SSL_PASSPHRASE', null),
+                'queue' => [
+                    'exchange' => 'wallets_exchange',
+                    'exchange_type' => 'topic',
+                    'exchange_routing_key' => 'wallets_rk',
+                    'prioritize_delayed' =>  false,
+                    'queue_max_priority' => 10,
+                    'job' => VladimirYuldashev\LaravelQueueRabbitMQ\Queue\Jobs\RabbitMQJob::class,
+                ],
+            ],
+        ],
+
+            /*
+             * Set to "horizon" if you wish to use Laravel Horizon.
+             */
+            'worker' => env('RABBITMQ_WORKER', 'default'),
+
+         ],
 
     ],
-
     /*
     |--------------------------------------------------------------------------
     | Failed Queue Jobs
